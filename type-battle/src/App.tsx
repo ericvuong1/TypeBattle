ecf96567991ca4ce773c9ccd396f86580361520e
ChengCalvin <calvincheng_95@hotmail.com>
2020-08-17 22:24:02 -0400

    add app.tsx change

--- type-battle/src/App.tsx
+++ type-battle/src/App.tsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import GamePage from "./components/Pages/GamePage";
 
-import io from "socket.io-client"
+import io from "socket.io-client";
 
-const socket = io('http://localhost:8080');
-
+const socket = io("http://localhost:8080");
 
 function App(): JSX.Element {
-
-  let [info, setInfo] = useState<string>("")
-  let [messages, setMessages] = useState<string[]>([])
+  let [info, setInfo] = useState<string>("");
+  let [messages, setMessages] = useState<string[]>([]);
   let [state, setState] = useState({});
 
-  socket.on('message', (text:string) => setMessages([...messages, text]));
-  socket.on('info', (text:string) => setInfo(text));
-  socket.on('state', (text:string) => setState(text));
+  socket.on("message", (text: string) => setMessages([...messages, text]));
+  socket.on("info", (text: string) => setInfo(text));
+  socket.on("state", (text: string) => setState(text));
 
   return (
     <div className="App">
+      <GamePage />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <header>{info}</header>
@@ -28,6 +28,5 @@ function App(): JSX.Element {
     </div>
   );
 }
-
 
 export default App;
