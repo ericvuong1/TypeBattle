1e37573dddcf632f342c48836f339da54b86b5c3
ericvuong1 <ericvuong22222@gmail.com>
2020-08-19 22:22:33 -0400

    Attempt to fix socket problem

--- type-battle/src/App.tsx
+++ type-battle/src/App.tsx
@@ -1,30 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import io from "socket.io-client"
 
-// const socket = io('http://localhost:8080');
-const socket = io('http://192.168.2.11:8080');
+// const socket = io('http://192.168.2.11:8080');
 
+const socket = io('http://localhost:8080');
+
+// TODO: Why put it outside function works?
+let messages: string[] = []
+
+const GameComponent = (): JSX.Element => {
+  let [info, setInfo] = useState<string>("")
+  let [state, setState] = useState({});
+
+  useEffect(() => {
+    socket.on('info', (text: string) => setInfo(text));
+    socket.on('state', (text: string) => setState(text));
+    socket.on('message', (text: string) => {
+      console.log(text, '456');
+      messages = [...messages, text]
+      console.log(messages, '789');
+    });
+  }, []);
+
+
+  const attack = (): void => {
+    const message = `${info}: Quick Attack`;
+    console.log(message);
+    socket.emit('message', message);
+    socket.emit('update', message);
+  }
+  console.log('loaded')
+
+  return (
+    <div>
+      <header>{info}</header>
+      <header>{messages}</header>
+      <header>{JSON.stringify(state)}</header>
+      <button onClick={attack}>hi</button>
+    </div>
+  )
+}
 
 function App(): JSX.Element {
 
-  let [info, setInfo] = useState<string>("")
-  let [messages, setMessages] = useState<string[]>([])
-  let [state, setState] = useState({});
 
-  socket.on('message', (text:string) => setMessages([...messages, text]));
-  socket.on('info', (text:string) => setInfo(text));
-  socket.on('state', (text:string) => setState(text));
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <header>{info}</header>
-        <header>{messages}</header>
-        <header>{JSON.stringify(state)}</header>
+        <GameComponent />
       </header>
     </div>
   );
