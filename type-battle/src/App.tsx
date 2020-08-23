42d99673484847d117f709e1733154f8641fc343
ericvuong1 <ericvuong22222@gmail.com>
2020-08-23 17:58:16 -0400

    Show example for typing UI

--- type-battle/src/App.tsx
+++ type-battle/src/App.tsx
@@ -8,16 +8,29 @@ import io from "socket.io-client"
 
 const socket = io('http://localhost:8080');
 
+type Player = {
+    hp: number
+    name: string
+    disabled: boolean
+    isInvulnerable: boolean
+    // spellQueue: Spell | undefined
+}
+
+type BoardState = {
+  "Player1": Player,
+  "Player2": Player
+}
+
 // TODO: Why put it outside function works?
 let messages: string[] = []
 
 const GameComponent = (): JSX.Element => {
-  let [info, setInfo] = useState<string>("")
-  let [state, setState] = useState({});
+  let [info, setInfo] = useState<"Player1" | "Player2" | "Player?">("Player?")
+  let [state, setState] = useState<BoardState | undefined >(undefined);
 
   useEffect(() => {
-    socket.on('info', (text: string) => setInfo(text));
-    socket.on('state', (text: string) => setState(text));
+    socket.on('info', (currentPlayer: "Player1" | "Player2") => setInfo(currentPlayer));
+    socket.on('state', (boardState: BoardState) => setState(boardState));
     socket.on('message', (text: string) => {
       messages = [...messages, text]
     });
@@ -40,7 +53,7 @@ const GameComponent = (): JSX.Element => {
         })}
       </li>
       <header>{info}</header>
-      <header>{JSON.stringify(state)}</header>
+      <header>{(state && info != "Player?") && state[info]['hp']}</header>
       <button onClick={castSpell('Quick Attack')}>Quick Attack</button>
       <button onClick={castSpell('Block')}>Block</button>
     </div>
